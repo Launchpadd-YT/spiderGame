6b93ecf480ef3072a2906adbbcb814c139ba097f
Mikko Morgan <mikko.morgan@icloud.com>
2024-02-01 23:48:34 -0500

    commit 2

--- spiderGame.py
+++ spiderGame.py
@@ -1,19 +1,18 @@
 # very nice game
 from tkinter import *
-from PIL import Image, ImageTk  # Import the Image and ImageTk modules from the PIL library
 import random
 
 # Create the window
 window = Tk()
-window.title("Spider Game")
+window.title("Lobotomization In porgess")
 
 # Create the canvas
 canvas = Canvas(window, width=425, height=450, bg="lightblue")
 canvas.pack()
 
 # Set up home screen with title and directions
-title = canvas.create_text(200, 200, text="Spider Game", fill="grey", font=("Helvetica", 25))
-directions = canvas.create_text(200, 300, text="Collect candy but avoid the purple orbs", fill="grey", font=("Helvetica", 17))
+title = canvas.create_text(200, 200, text="Lobotomizer", fill="grey", font=("Helvetica", 25))
+directions = canvas.create_text(200, 300, text="Collect difficulties but avoid the exterm demone", fill="grey", font=("Helvetica", 17))
 
 # Score setup
 score = 0
@@ -26,55 +25,76 @@ lvlDisp = Label(window, text="Level :" " " + str(lvl))
 lvlDisp.pack()
 
 # Load the player character image
-playerImg = Image.open("greenChar.gif")
-playerImg = playerImg.resize((30, 30), Image.ANTIALIAS)  # Resize the image to 30x30 pixels
-playerImg = ImageTk.PhotoImage(playerImg)  # Convert the image to a format that Tkinter can use
+img = PhotoImage(file = "normal.png")
+playerImg = img.subsample(30, 30)
+
+img2 = PhotoImage(file = "auto.png")
+auto = img2.subsample(33, 33)
+
+img3 = PhotoImage(file = "easy.png")
+easy = img3.subsample(25, 25)
+
+img4 = PhotoImage(file = "hard.png")
+hard = img4.subsample(30, 30)
+
+img5 = PhotoImage(file = "harder.png")
+harder = img5.subsample(30, 30)
+
+img6 = PhotoImage(file = "insane.png")
+insane = img6.subsample(30, 30)
+
+img7 = PhotoImage(file = "extremeDem.png")
+extremeDem = img7.subsample(10, 10)
+
+
+
 
 # Create the player character on the canvas
 mainChar = canvas.create_image(200, 360, image=playerImg)
 
-#vars + list 4 candy
-candyList = []
-avoidCandy = []
-candySpeed = 2
-candyColList = ["SeaGreen", "Cyan", "purple4", "DodgeBlue", "hotpink", "gold1", "salmon", "snow", "SpringGreen"]
+#vars + list 4 face
+faceList = []
+avoidFace = []
+faceSpeed = 2
+faceImgList = [auto, easy, hard, harder, insane, extremeDem]
 
-def makeCandy():
+def makeFace():
     #random x pos
     xpos = random.randint(1, 400)
     #random color
-    candyCol = random.choice(candyColList)
-    #create size 30 candy w/ selected col & pos
-    candy = canvas.create_oval(xpos, 0, xpos + 30, 30, fill = candyCol)
-    #add candy to list but if purple add 2 avoidCandy
-    if candyCol == "purple4":
-        avoidCandy.append(candy)
+    faceImg = random.choice(faceImgList)
+    #create size 30 face w/ selected col & pos
+    face = canvas.create_image(xpos, 0, image = faceImg)
+    #add face to list but if extreme add 2 avoidFace
+    if faceImg == extremeDem:
+        avoidFace.append(face)
+        faceList.append(face)
     else:
-        candyList.append(candy)
+        faceList.append(face)
     #schedule this func. again
-    window.after(1000, makeCandy)
+    window.after(1000, makeFace)
 
-#func. moves candy down
-def moveCandy():
-    #loop thru list of candy
-    for candy in candyList:
-        canvas.move(candy, 0, candySpeed)
-        if canvas.coords(candy)[1] > 400:
+#func. moves face down
+def moveFace():
+    #loop thru list of face
+    for face in faceList:
+        canvas.move(face, 0, faceSpeed)
+        if canvas.coords(face)[1] > 400:
             xpos = random.randint(1, 400)
-            canvas.coords(candy, xpos, 0, xpos + 30, 30)
-    window.after(50, moveCandy)
+            canvas.coords(face, xpos, 0)
+    window.after(50, moveFace)
 
 def updateScoreLvl():
-    global score, lvl, candySpeed
+    global score, lvl, faceSpeed
     score += 1
     scoreDisp.config(text = " Score :" + str(lvl))
     #update lvl + speed
     if score > 5 and score <= 10:
-        candySpeed += 1
+        faceSpeed += 1
         lvl = 2
         lvlDisp.config(text = "Level :" + str(lvl))
     elif score > 10:
-        candySpeed += 1
+        faceSpeed += 1
         lvl = 3
         lvlDisp.config(text = "Level :" + str(lvl))
 
@@ -93,21 +113,21 @@ def collision(item1, item2, dist):
     overlap = xDist < dist and yDist < dist
     return overlap
 
-#checks if char hit avoid candy, schedule game over
-#if char hits candy, remove from screen, list, update score
+#checks if char hit avoid face, schedule game over
+#if char hits face, remove from screen, list, update score
 def checkHits():
-    for candy in avoidCandy:
-        if collision(mainChar, candy, 30):
+    for face in avoidFace:
+        if collision(mainChar, face, 30):
             gameOver = canvas.create_text(200, 200, text = "Game Over", fill = "royalblue4", font = ("Helvetica", 25))
             window.after(2000, gameOver) #end game + let user see score
-            # dont check any other candy, window will be destroyed
+            # dont check any other face, window will be destroyed
             return
-    #check if good candy hit
-    for candy in candyList:
-        if collision(mainChar, candy, 30):
-            canvas.delete(candy) #remove from canvas
+    #check if good face hit
+    for face in faceList:
+        if collision(mainChar, face, 30):
+            canvas.delete(face) #remove from canvas
             #find where in list, remove, update score
-            candyList.remove(candy)
+            faceList.remove(face)
             updateScoreLvl()
     #schedule check hits again
     window.after(100, checkHits)
@@ -136,10 +156,10 @@ def moveChar():
 
 #bind keys to the char
 canvas.bind_all("<KeyPress>", checkInput) #bind keypress
-canvas.bind_all("<KeyRelease", endInput)
+canvas.bind_all("<KeyRelease>", endInput)
 window.after(1000, endTitle)
-window.after(1000, makeCandy)
-window.after(1000, moveCandy)
+window.after(1000, makeFace)
+window.after(1000, moveFace)
 window.after(1000, checkHits)
 window.after(1000, moveChar)
 window.mainloop()
